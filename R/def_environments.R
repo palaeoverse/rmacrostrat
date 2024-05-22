a2dcd4e3af944133de0d6c5a909e7b522c6e1167
Christopher Dean <christopherdaviddean@gmail.com>
2024-05-22 11:27:40 +0200

    Updated description and code for defs_environments.

--- R/def_environments.R
+++ R/def_environments.R
@@ -1,40 +1,56 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param environ \code{character}. The name of a specific environment (e.g., "reef") to return a definition for.
-#' @param environ_type \code{character}. The name of a type of environment (e.g., "carbonate") to return a definition for.
-#' @param environ_class \code{character}. The name of a class of envrionment (e.g., "marine") to return a definition for.
-#' @param environ_id \code{integer}. The unique identification number(s) of the desired environment(s) to return a definition for.
-#' @return OUTPUT_DESCRIPTION
-#' @author AUTHOR [AUTHOR_2]
-#' @details DETAILS
+#' @title Retrieve environment definitions
+#' @description A function to retrieve the definitions for one or more
+#'   environments in the Macrostrat database. By default, all environment
+#'   definitions are returned.
+#' @param environ \code{character}. The name of a specific environment (e.g.,
+#'   "reef") to return a definition for.
+#' @param environ_type \code{character}. The name of a type of environment
+#'   (e.g., "carbonate") to return a definition for.
+#' @param environ_class \code{character}. The name of a class of envrionment
+#'   (e.g., "marine") to return a definition for.
+#' @param environ_id \code{integer}. The unique identification number of the
+#'   desired environment to return a definition for.
+#' @return A `data.frame` containing the following columns:
+#' \itemize{
+#'   \item \code{environ_id}: The unique identification number of the environment.
+#'   \item \code{name}: The name of the environment.
+#'   \item \code{type}: Environment type, less inclusive than class.
+#'   \item \code{class}: Environment class, more inclusive than type.
+#'   \item \code{color}: Recommended coloring for environment, based on the
+#'   dominant lithology.
+#'   \item \code{t_units}: The total number of Macrostrat units that are
+#'     partially or entirely composed of the environment.
+#' }
+#' @author Christopher D. Dean [AUTHOR_2]
+#' @reviewer N/A
 #' @examples
 #' \dontrun{
 #' if (interactive()) {
-#'   # EXAMPLE1
+#' # return all environment definitions
+#' ex1 <- def_environments()
+#' # return subsets of environment definitions
+#' ex2 <- def_environments(environ = "reef")
+#' ex3 <- def_environments(environ_type = "carbonate")
+#' ex4 <- def_environments(environ_class = "marine")
 #' }
 #' }
 #' @export
 
-def_environments <- function(
-    environ = NULL,
-    environ_type = NULL,
-    environ_class = NULL,
-    environ_id = NULL) {
+def_environments <- function(environ = NULL, environ_type = NULL,
+    environ_class = NULL, environ_id = NULL) {
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
-  ref <- list(
-    environ = "character",
-    environ_type = "character",
-    environ_class = "character",
-    environ_id = "integer"
+  ref <- list(environ = "character", environ_type = "character",
+    environ_class = "character", environ_id = "integer"
   )
   check_arguments(x = args, ref = ref)
   # Set default for format
   format <- "json"
   # Get request
-  dat <- GET_macrostrat(endpoint = "INSERT ENDPOINT", query = args, format = format)
+  dat <- GET_macrostrat(endpoint = "defs/environments", query = args,
+                        format = format)
 
   # Return data
   return(dat)
