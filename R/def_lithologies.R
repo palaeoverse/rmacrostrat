89bcd82ed7a911137cb9ab8daa97c92a2bade24f
William Gearty <willgearty@gmail.com>
2024-05-22 10:03:55 +0200

    Complete def_lithologies function

--- R/def_lithologies.R
+++ R/def_lithologies.R
@@ -1,43 +1,66 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param lithology \code{character}. The name of a lithology (e.g., "limestone") to return a definition for.
-#' @param lithology_group \code{character}. The name of a lithological group (e.g., "unconsolidated") to return a definition for.
-#' @param lithology_class \code{character}. The name of a lithological class (e.g., "sedimentary") to return a definition for.
-#' @param lithology_type \code{character}. The name of a lithological type (e.g., "siliciclastic") to return a definition for.
-#' @param lithology_id \code{integer}. The unique identification number(s) of one or more lithologies to return a definition for.
-#' @return OUTPUT_DESCRIPTION
-#' @author AUTHOR [AUTHOR_2]
-#' @details DETAILS
+#' @title Retrieve lithology definitions
+#' @description A function to retrieve the definitions for one or more
+#'   lithologies in the Macrostrat database. By default, all lithology
+#'   definitions are returned.
+#' @param lithology \code{character}. The name of a lithology (e.g.,
+#'   "limestone") to return a definition for.
+#' @param lithology_group \code{character}. The name of a lithological group
+#'   (e.g., "unconsolidated") to return a definition for.
+#' @param lithology_class \code{character}. The name of a lithological class
+#'   (e.g., "sedimentary") to return a definition for.
+#' @param lithology_type \code{character}. The name of a lithological type
+#'   (e.g., "siliciclastic") to return a definition for.
+#' @param lithology_id \code{integer}. The unique identification number(s) of
+#'   one or more lithologies to return a definition for.
+#' @return A `data.frame` containing the following columns:
+#' \itemize{
+#'   \item \code{lith_id}: Unique ID of the lithology.
+#'   \item \code{name}: The name of the lithology.
+#'   \item \code{type}: Lithology type, less inclusive than class.
+#'   \item \code{group}: Lithology group, less inclusive than type.
+#'   \item \code{class}: Lithology class, more inclusive than type.
+#'   \item \code{color}: Recommended coloring for lithology.
+#'   \item \code{fill}: The code for the fill pattern as established in the
+#'     [Federal Geographic Data Committee](https://ngmdb.usgs.gov/fgdc_gds/)'s
+#'     [Digital Cartographic Standard for Geologic Map
+#'     Symbolization](https://ngmdb.usgs.gov/fgdc_gds/).
+#'   \item \code{t_units}: The total number of Macrostrat units that are
+#'     partially or entirely composed of the lithology.
+#' }
+#' @author William Gearty
 #' @examples
 #' \dontrun{
-#' if (interactive()) {
-#'   # EXAMPLE1
-#' }
+#' # return all lithology definitions
+#' ex1 <- def_lithologies()
+#' # return subsets of lithology definitions
+#' ex2 <- def_lithologies(lithology = "sandstone")
+#' ex3 <- def_lithologies(lithology_type = "sedimentary")
+#' ex4 <- def_lithologies(lithology_id = c(1,5))
 #' }
 #' @export
-
-def_lithologies <- function(
-    lithology = NULL,
-    lithology_group = NULL,
-    lithology_class = NULL,
-    lithology_type = NULL,
-    lithology_id = NULL) {
+def_lithologies <- function(lithology = NULL, lithology_group = NULL,
+                            lithology_class = NULL, lithology_type = NULL,
+                            lithology_id = NULL) {
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
-  ref <- list(
-    lithology = "character",
-    lithology_group = "character",
-    lithology_class = "character",
-    lithology_type = "character",
-    lithology_id = "integer"
+  ref <- list(lithology = "character", lithology_group = "character",
+              lithology_class = "character", lithology_type = "character",
+              lithology_id = "integer"
   )
   check_arguments(x = args, ref = ref)
-  # Set default for format
-  format <- "json"
+  # Change arguments to default api names
+  api_names <- c(lithology = "lith", lithology_class = "lith_class",
+                 lithology_group = "lith_group", lithology_id = "lith_id",
+                 lithology_type = "lith_type")
+  # Match names
+  rpl <- match(x = names(api_names), table = names(args))
+  # Replace names
+  names(args)[rpl] <- as.vector(unlist(api_names))
   # Get request
-  dat <- GET_macrostrat(endpoint = "INSERT ENDPOINT", query = args, format = format)
+  dat <- GET_macrostrat(endpoint = "defs/lithologies", query = args,
+                        format = "json")
 
   # Return data
   return(dat)
