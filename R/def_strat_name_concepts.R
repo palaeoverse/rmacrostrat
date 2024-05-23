d9bb153116432a4971b7f2b2fa4d8cdae37a66da
Christopher Dean <christopherdaviddean@gmail.com>
2024-05-23 12:37:55 +0200

    Updated description and code for 'def_strat_name_concepts()' and associated manual.

--- R/def_strat_name_concepts.R
+++ R/def_strat_name_concepts.R
@@ -1,35 +1,80 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param strat_concept_name \code{character}. The name(s) of the desired stratigraphic name concept(s) to return a definition for (e.g., "Hell Creek").
-#' @param strat_concept_id \code{integer}. The unique identification number(s) of the desired stratigraphic name concept(s) to return a definition for.
-#' @return OUTPUT_DESCRIPTION
-#' @author AUTHOR [AUTHOR_2]
-#' @details DETAILS
+#' @title Retrieve stratigraphic name concept definitions
+#' @description A function to retrieve the definitions for stratigraphic name
+#' concepts. By default, all stratigraphic name concept definitions
+#' are returned.
+#' @param strat_concept_name \code{character}. The name(s) of the desired
+#'   stratigraphic name concept(s) to return a definition for (e.g., "Hell
+#'   Creek").
+#' @param strat_concept_id \code{integer}. The unique identification number(s)
+#'   of the desired stratigraphic name concept(s) to return a definition for.
+#' @return A `data.frame` containing the following columns:
+#' \itemize{
+#'   \item \code{concept_id}: The unique identification number for the
+#'   stratigraphic name concept, which groups variant `strat_name`s for same
+#'   geologic entity.
+#'   \item \code{name}: The name of the stratigraphic name concept.
+#'   \item \code{geologic_age}: The geologic age information assigned to the
+#'   stratigraphic name concept, as defined in the original reference in plain
+#'   text.
+#'   \item \code{int_id}: The unique identification number of the
+#'   chronostratigraphic interval associated with the stratigraphic name concept
+#'   (see [def_intervals()]).
+#'   \item \code{b_int_id}: The unique identification number of the bottom
+#'   (oldest) chronostratigraphic interval associated with the stratigraphic
+#'   name concept (see [def_intervals()]).
+#'   \item \code{t_int_id}: The unique identification number of the top
+#'   (youngest) chronostratigraphic interval associated with the stratigraphic
+#'   name concept (see [def_intervals()]).
+#'   \item \code{useage_notes}: Notes on the current validity of the
+#'   stratigraphic name concept, in plain text.
+#'   \item \code{other}: Other associated notes on the stratigraphic name
+#'   concept, in plain text.
+#'   \item \code{province}: The provinces where the stratigraphic name concept
+#'   appears, in plain text.
+#'   \item \code{refs}: The unique identification number of the reference from
+#'   which the stratigraphic name concept was sourced (see [def_refs()]).
+#'   \item \code{url}: The URL associated with the stratigraphic name concept,
+#'   where additional information, the source or contributing publication can be
+#'   found.
+#'   \item \code{author}: The authors of the source reference.
+#'   }
+#' @author Christopher D. Dean [AUTHOR_2]
+#' @section Reviewer: N/A
+#' @details Stratigraphic name concepts consist of grouped `strat_name`s which
+#' represent the same geologic entity. This may be due to stratigraphic names
+#' appearing in multiple reference maps, or at multiple lithostratigraphic
+#' ranks.
 #' @examples
 #' \dontrun{
-#' if (interactive()) {
-#'   # EXAMPLE1
-#' }
+#' # Return a stratigraphic name concept based on a string
+#' ex1 <- def_strat_name_concepts(strat_concept_name = "Dakota")
+#' # Return a stratigraphic name concept based on an ID
+#' ex1 <- def_strat_name_concepts(strat_concept_id = 8212)
 #' }
 #' @export
 
-def_strat_name_concepts <- function(
-    strat_concept_name = NULL,
-    strat_concept_id = NULL) {
+def_strat_name_concepts <- function(strat_concept_name = NULL,
+                                    strat_concept_id = NULL) {
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
-  ref <- list(
-    strat_concept_name = "character",
-    strat_concept_id = "integer"
+  ref <- list(strat_concept_name = "character",
+              strat_concept_id = "integer"
   )
   check_arguments(x = args, ref = ref)
+  # Recode names
+  api_names <- list(strat_concept_id = "concept_id",
+                    strat_concept_name = "concept_name")
+  # Match names
+  rpl <- match(x = names(api_names), table = names(args))
+  # Replace names
+  names(args)[rpl] <- as.vector(unlist(api_names))
   # Set default for format
   format <- "json"
   # Get request
-  dat <- GET_macrostrat(endpoint = "INSERT ENDPOINT", query = args, format = format)
-
+  dat <- GET_macrostrat(endpoint = "defs/strat_name_concepts", query = args,
+                        format = format)
   # Return data
   return(dat)
 }
