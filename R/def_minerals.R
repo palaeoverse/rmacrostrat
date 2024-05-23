5198bf650e7d921b8a54aaee0a1d6feab3da6feb
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-05-23 12:06:04 +0200

    Update def_minerals.R

--- R/def_minerals.R
+++ R/def_minerals.R
@@ -1,23 +1,49 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param mineral \code{character}. The name of a mineral (e.g., "calcite") to return a definition for.
-#' @param mineral_type \code{character}. The type of mineral (e.g., "feldspar") to return a definition for.
-#' @param element \code{character}. The chemical element abbreviation of a mineral to return a definition for (e.g., "CaCO3"). Note: abbreviations are case sensitive.
-#' @return OUTPUT_DESCRIPTION
-#' @author AUTHOR [AUTHOR_2]
-#' @details DETAILS
+#' @title Retrieve mineral definitions
+#'
+#' @description A function to retrieve mineral definitions from the Macrostrat
+#'   database based on user-specified arguments. If no arguments are provided,
+#'   all definitions are returned.
+#'
+#' @param mineral \code{character}. The name of a mineral (e.g., "calcite") to
+#'   return a definition for.
+#' @param mineral_type \code{character}. The type of mineral (e.g.,
+#'   "feldspar") to return a definition for.
+#' @param element \code{character}. The chemical element abbreviation of a
+#'   mineral to return a definition for (e.g., "CaCO3"). Note: abbreviations
+#'   are case sensitive.
+#'
+#' @return A \code{data.frame} containing the following columns:
+#' \itemize{
+#'   \item \code{mineral_id}: The unique identification number of the mineral.
+#'   \item \code{mineral}: The name of the mineral.
+#'   \item \code{mineral_type}: The name of the type of mineral.
+#'   \item \code{hardness_min}: The minimum hardness value (Moh's hardness
+#'   scale) of the mineral.
+#'   \item \code{hardness_max}: The maximum hardness value (Moh's hardness
+#'   scale) of the mineral.
+#'   \item \code{mineral_color}: The color description of the mineral.
+#'   \item \code{lustre}: The lustre description of the mineral.
+#'   \item \code{crystal_form}: The crystal form of the mineral.
+#'   \item \code{formula}: The chemical formula of the mineral.
+#'   \item \code{formula_tags}: The chemical formula of the mineral with
+#'   appropriate sub/superscript html tags.
+#'   \item \code{url}: A link to where additional information on the mineral
+#'   can be found.
+#' }
+#'
+#' @author Lewis A. Jones
+#'
 #' @examples
 #' \dontrun{
-#' if (interactive()) {
-#'   # EXAMPLE1
-#' }
+#'   # Get all defintions
+#'   ex1 <- def_minerals()
+#'   # Get mineral by type
+#'   ex2 <- def_minerals(mineral_type = "element")
 #' }
 #' @export
-
-def_minerals <- function(
-    mineral = NULL,
-    mineral_type = NULL,
-    element = NULL) {
+def_minerals <- function(mineral = NULL,
+                         mineral_type = NULL,
+                         element = NULL) {
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
@@ -28,11 +54,9 @@ def_minerals <- function(
     element = "character"
   )
   check_arguments(x = args, ref = ref)
-  # Set default for format
-  format <- "json"
   # Get request
-  dat <- GET_macrostrat(endpoint = "INSERT ENDPOINT", query = args, format = format)
-
+  dat <- GET_macrostrat(endpoint = "defs/minerals",
+                        query = args, format = "json")
   # Return data
   return(dat)
 }
