fe3b55f35ed2bd5a197994df434a25ee27bc484b
William Gearty <willgearty@gmail.com>
2024-05-23 11:57:20 +0200

    Complete def_structures function

--- R/def_structures.R
+++ R/def_structures.R
@@ -1,40 +1,46 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param structure \code{character}. The name of a geological structure (e.g., "syncline") to return a definition for.
-#' @param structure_class \code{character}. The name of the geological structure class (e.g., "sedimentology") to return a definition for.
-#' @param structure_type \code{character}. The name of the geological structure type (e.g., "fault") to return a definition for.
-#' @param structure_id \code{integer}. The unique idenfication number(s) of geological structures(s) to return a definition for.
-#' @return OUTPUT_DESCRIPTION
-#' @author AUTHOR [AUTHOR_2]
-#' @details DETAILS
+#' @title Retrieve geological structure definitions
+#' @description A function to retrieve definitions for various geological
+#'   structures within the Macrostrat database.
+#' @param structure \code{character}. The name of a geological structure (e.g.,
+#'   "syncline") to return a definition for.
+#' @param structure_class \code{character}. The name of the geological structure
+#'   class (e.g., "sedimentology") to return a definition for.
+#' @param structure_type \code{character}. The name of the geological structure
+#'   type (e.g., "fault") to return a definition for.
+#' @param structure_id \code{integer}. The unique idenfication number(s) of
+#'   geological structures(s) to return a definition for.
+#' @return A \code{data.frame} containing the following columns:
+#' \itemize{
+#'   \item \code{structure_id}: The unique identification number of the
+#'     structure.
+#'   \item \code{name}: The name of the structure.
+#'   \item \code{structure type}: Structure type, less inclusive than class.
+#'   \item \code{group}: Structure group, less inclusive than type.
+#'   \item \code{class}: Structure class, more inclusive than type.
+#' }
+#' @author William Gearty
 #' @examples
 #' \dontrun{
-#' if (interactive()) {
-#'   # EXAMPLE1
-#' }
+#' # get all structure definitions
+#' ex1 <- def_structures()
+#' # get subset of structure definitions
+#' ex2 <- def_structures(structure_id = c(1, 3))
+#' ex3 <- def_structures(structure_class = "igneous")
 #' }
 #' @export
-
-def_structures <- function(
-    structure = NULL,
-    structure_class = NULL,
-    structure_type = NULL,
-    structure_id = NULL) {
+def_structures <- function(structure = NULL, structure_class = NULL,
+                           structure_type = NULL, structure_id = NULL) {
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
-  ref <- list(
-    structure = "character",
-    structure_class = "character",
-    structure_type = "character",
-    structure_id = "integer"
+  ref <- list(structure = "character", structure_class = "character",
+              structure_type = "character", structure_id = "integer"
   )
   check_arguments(x = args, ref = ref)
-  # Set default for format
-  format <- "json"
   # Get request
-  dat <- GET_macrostrat(endpoint = "INSERT ENDPOINT", query = args, format = format)
+  dat <- GET_macrostrat(endpoint = "defs/structures", query = args,
+                        format = "json")
 
   # Return data
   return(dat)
