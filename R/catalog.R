848cd0b39e1f0f9b34ce4accd3ab2f823940e2a4
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-05-23 14:20:08 +0200

    add catalog

--- R/catalog.R
+++ R/catalog.R
@@ -1,9 +1,11 @@
 #' @title Retrieve a complete set of definitions
+#'
 #' @description A function to retrieve all definitions and/or metadata for a
-#' given definition set. Caution: some datasets are large and may take time to
-#' retrieve.
+#'   given definition set. Caution: some datasets are large and may take time
+#'   to retrieve.
 #'
 #' @param type \code{character}. The name of a definition set to retrieve.
+#'
 #' Options are:
 #' \describe{
 #'  \item{columns}{Definitions of Macrostrat columns. See also
@@ -25,7 +27,8 @@
 #'  [def_measurements()].}
 #'  \item{minerals}{Defintions of minerals. See also [def_minerals()].}
 #'  \item{plates}{Definitions of tectonic plates. See also [def_plates()].}
-#'  \item{projects}{Metadata on Macrostrat projects. See also [def_projects()].}
+#'  \item{projects}{Metadata on Macrostrat projects. See also
+#'  [def_projects()].}
 #'  \item{references}{Metadata on references. See also [def_references()].}
 #'  \item{sources}{Metadata on geological maps. See also [def_sources()].}
 #'  \item{strat_name_concepts}{Metadata on stratigraphic name concepts. See also
@@ -37,39 +40,23 @@
 #'  \item{timescales}{Metadata on timescales. See also [def_timescales()].}
 #'  }
 #'
-#' @return A \code{dataframe} containing an exhaustive list of the definitions
-#' and/or metadata retrievable for the given definition set. The variables in
-#' the \code{dataframe} will match those described in the documentation for
-#' that definition set.
+#' @return A \code{data.frame} containing an exhaustive list of the
+#'   definitions and/or metadata retrievable for the given definition set. The
+#'   variables in the \code{data.frame} will match those described in the
+#'   documentation for that definition set.
 #'
-#' @author Bethany Allen [AUTHOR_2]
+#' @author Bethany Allen & Lewis A. Jones
 #'
 #' @examples
 #' \dontrun{
-#' if (interactive()) {
-#'   # mineral_definitions <- catalogue(type = "minerals")
-#' }
+#'   # Get mineral definitions
+#'   ex1 <- catalog(type = "minerals")
+#'   # Get grain size definitions
+#'   ex2 <- catalog(type = "grain_sizes")
 #' }
 #' @export
-
-catalogue <- function(
-    type = NULL) {
-
-  # Error handling
-  # Collect input arguments as a list
-  args <- as.list(environment())
-  # Check whether class of arguments is valid
-  ref <- list(
-    type = "character"
-  )
-  check_arguments(x = args, ref = ref)
-
-  # Set default for format
-  format <- "json"
-
-  # Get request
-  dat <- GET_macrostrat(endpoint = "defs/econs", query = args, format = format)
-
-  # Return data
+catalog <- function(type = NULL) {
+  if (!is.character(type)) stop("`type` must be of class character.")
+  dat <- get(paste0("def_", type))()
   return(dat)
 }
