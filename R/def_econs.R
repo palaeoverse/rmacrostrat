337f36e95677a75c9616ad4237c2a2b47f27649d
Bethany Allen <bethany.allen@bsse.ethz.ch>
2024-05-23 11:34:51 +0200

    Update def_econs

--- R/def_econs.R
+++ R/def_econs.R
@@ -1,16 +1,36 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param econ \code{character}. The name of the economic resource (e.g., "gas reservoir") to return a definition for.
-#' @param econ_type \code{character}. The type of economic resource (e.g., "hydrocarbon") to return a definition for.
-#' @param econ_class \code{character}. The class of economic resource (e.g., "energy") to return a definition for.
-#' @param econ_id \code{integer}. The unique idenfication number(s) of the economic resource(s) to return a definition for.
-#' @return OUTPUT_DESCRIPTION
-#' @author AUTHOR [AUTHOR_2]
-#' @details DETAILS
+#' @title Retrieve economic definitions
+#' @description A function to retrieve the definitions for one or more
+#'   economic resources in the Macrostrat database. By default, all economic
+#'   definitions are returned.
+#'
+#' @param econ \code{character}. The name of the economic resource (e.g., "gas
+#'   reservoir") to return a definition for.
+#' @param econ_type \code{character}. The type of economic resource (e.g.,
+#'   "hydrocarbon") to return a definition for.
+#' @param econ_class \code{character}. The class of economic resource (e.g.,
+#'   "energy") to return a definition for.
+#' @param econ_id \code{integer}. The unique identification number(s) of the
+#'   economic resource(s) to return a definition for.
+#'
+#' @return A \code{dataframe} containing:
+#' \describe{
+#'  \item{econ_id}{The unique identifier of the economic resource}
+#'  \item{name}{The name of the economic resource}
+#'  \item{type}{The type of economic resource}
+#'  \item{class}{The class of economic resource}
+#'  \item{color}{The recommended coloring for units based on the dominant
+#'  lithology}
+#'  \item{t_units}{The total number of Macrostrat units containing the economic
+#'  resource}
+#'  }
+#'
+#' @author Bethany Allen [AUTHOR_2]
+#'
 #' @examples
 #' \dontrun{
 #' if (interactive()) {
-#'   # EXAMPLE1
+#'   # econ_inf <- def_econs()
+#'   # econ_inf <- def_econs(econ_type = "hydrocarbon")
 #' }
 #' }
 #' @export
@@ -20,6 +40,7 @@ def_econs <- function(
     econ_type = NULL,
     econ_class = NULL,
     econ_id = NULL) {
+
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
@@ -31,10 +52,12 @@ def_econs <- function(
     econ_id = "integer"
   )
   check_arguments(x = args, ref = ref)
+
   # Set default for format
   format <- "json"
+
   # Get request
-  dat <- GET_macrostrat(endpoint = "INSERT ENDPOINT", query = args, format = format)
+  dat <- GET_macrostrat(endpoint = "defs/econs", query = args, format = format)
 
   # Return data
   return(dat)
