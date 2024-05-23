42bb12ec5d23229632c8525241dce445598bf1f9
William Gearty <willgearty@gmail.com>
2024-05-23 11:43:01 +0200

    Complete def_measurements function

--- R/def_measurements.R
+++ R/def_measurements.R
@@ -1,37 +1,48 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param measure_id \code{integer}. The unique identification number(s) of the desired measurement definition(s) to return a definition for.
-#' @param measurement_type \code{character}. The type of measurement definitions to return a definition for (e.g., "geochronological"). This is a less inclusive grouping than `class`.
-#' @param measurement_class \code{character}. The class of measurement definitions to return a definition for (e.g., "geochemical"). This is a more inclusive grouping than `type`.
-#' @return OUTPUT_DESCRIPTION
-#' @author AUTHOR [AUTHOR_2]
-#' @details DETAILS
+#' @title Retrieve measurement definitions
+#' @description A function to retrieve the definitions of different measurements
+#'   that are included in the Macrostrat database. By default, all definitions
+#'   are returned.
+#' @param measure_id \code{integer}. The unique identification number(s) of the
+#'   desired measurement(s) to return a definition for.
+#' @param measurement_type \code{character}. Filter the returned definitions to
+#'   those of one or more named type(s) of measurement (e.g.,
+#'   "geochronological"). This is a less inclusive grouping than
+#'   `measurement_class`.
+#' @param measurement_class \code{character}. Filter the returned definitions to
+#'   those of one or more named class(es) of measurement (e.g., "geochemical").
+#'   This is a more inclusive grouping than `measurement_type`.
+#' @return A \code{data.frame} containing the following columns:
+#' \itemize{
+#'   \item \code{measure_id}: Unique identification number of the measurement.
+#'   \item \code{name}: The name of the measurement.
+#'   \item \code{type}: Measurement type, less inclusive than class
+#'   \item \code{class}: Measurement class, more inclusive than type.
+#'   \item \code{t_units}: Total number of Macrostrat units containing this
+#'     measurement.
+#' }
+#' @author William Gearty
 #' @examples
 #' \dontrun{
-#' if (interactive()) {
-#'   # EXAMPLE1
-#' }
+#' # get all definitions
+#' ex1 <- def_measurements()
+#' # get a subset of definitions
+#' ex2 <- def_measurements(measure_id = c(1, 2, 4))
+#' ex3 <- def_measurements(measurement_class = "geochemical")
 #' }
 #' @export
 
-def_measurements <- function(
-    measure_id = NULL,
-    measurement_type = NULL,
-    measurement_class = NULL) {
+def_measurements <- function(measure_id = NULL, measurement_type = NULL,
+                             measurement_class = NULL) {
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
-  ref <- list(
-    measure_id = "integer",
-    measurement_type = "character",
-    measurement_class = "character"
-  )
+  ref <- list(measure_id = "integer", measurement_type = "character",
+              measurement_class = "character")
   check_arguments(x = args, ref = ref)
-  # Set default for format
-  format <- "json"
   # Get request
-  dat <- GET_macrostrat(endpoint = "INSERT ENDPOINT", query = args, format = format)
+  dat <- GET_macrostrat(endpoint = "defs/measurements", query = args,
+                        format = "json")
 
   # Return data
   return(dat)
