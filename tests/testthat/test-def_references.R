c581c41ee7fb02274d3b9e93cde84f2c10fd6d1b
William Gearty <willgearty@gmail.com>
2024-05-24 13:25:41 +0200

    Tests for last def_ functions

--- tests/testthat/test-def_references.R
+++ tests/testthat/test-def_references.R
@@ -1,3 +1,10 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("def_references works", {
+  rmacrostrat_test_checks(fn = def_references,
+                          check_args = list(ref_id = c(1, 2, 3)),
+                          fail_args = list(# wrong argument
+                                           ref_group = 70,
+                                           # wrong type
+                                           ref_id = "test"),
+                          warn_args = list(ref_id = -604),
+                          col_no = 7, check_null = TRUE)
 })
--- tests/testthat/test-def_structures.R
+++ tests/testthat/test-def_structures.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("def_structures works", {
+  rmacrostrat_test_checks(fn = def_structures,
+                          check_args = list(structure_id = c(1, 3),
+                                            structure_class = "igneous"),
+                          fail_args = list(# wrong argument
+                                           structure_group = 70,
+                                           # wrong type
+                                           structure_id = "test"),
+                          warn_args = list(structure_id = -604),
+                          col_no = 5, check_null = TRUE)
 })
