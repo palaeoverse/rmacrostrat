3028e962745b54f1f4f032b2154ee0b702d18366
Christopher Dean <christopherdaviddean@gmail.com>
2024-05-29 14:13:27 +0100

    Updated def_plates() to fit suggested guidelines.

--- R/def_plates.R
+++ R/def_plates.R
@@ -1,4 +1,4 @@
-#' @title Retrieve tectonic plate definition
+#' @title Define tectonic plates
 #'
 #' @description A function to retrieve tectonic plate definitions for a
 #'   user-specified plate identification number, as defined by the Wright et
@@ -8,17 +8,16 @@
 #' @param plate_id \code{integer}. The unique identification number(s) of the
 #'   desired plate(s) to return a definition for. If NULL (default), all
 #'   plates are returned.
-#'
 #' @return A `data.frame` containing the `plate_id` of the tectonic plate and
 #'   the name of the tectonic plate as defined by Wright et al. (2013).
-#'
-#' @author Lewis A. Jones
-#'
+#' @section Developer(s):
+#'  Lewis A. Jones
+#' @section Reviewer(s):
+#'  Christopher D. Dean
 #' @section References: Wright, N., Zahirovic, S., Müller, R.D., Seton, M.
 #'   (2013). Towards community-driven paleogeographic reconstructions:
 #'   integrating open-access paleogeographic and paleobiology data with plate
 #'   tectonics. Biogeosciences, 10, 1529--1541. \doi{10.5194/bg-10-1529-2013}.
-#'
 #' @examples
 #' \dontrun{
 #'  # Get all tectonic plates
