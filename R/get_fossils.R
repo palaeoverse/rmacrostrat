1b7d86c51130823665130ef5374ec3b0414d6041
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-05-29 16:15:43 +0200

    suggested changes fossils

--- R/get_fossils.R
+++ R/get_fossils.R
@@ -1,6 +1,8 @@
 #' @title Retrieve collections from the Paleobiology Database
+#'
 #' @description A function to retrieve collections from the [Paleobiology
-#' Database](http://paleobiodb.org), matched to Macrostrat units.
+#' Database](https://paleobiodb.org/#/) (PBDB), matched to Macrostrat units.
+#'
 #' @param unit_id \code{integer}. Filter PBDB collections to those within one or
 #'   more unit(s) as specified by their unique identification number(s).
 #' @param column_id \code{integer}. Filter PBDB collections to those within one
@@ -12,11 +14,11 @@
 #' @param age_top \code{numeric}. Filter PBDB collections to those that overlap
 #'   with the age range between the specified numerical age and `age_bottom`.
 #'   Should be in millions of years before present. `age_bottom` must also be
-#'   specified, and this must be younger than `age_bottom`.
+#'   specified, and this must be older than `age_top`.
 #' @param age_bottom \code{numeric}. Filter PBDB collections to those that
 #'   overlap with the age range between the specified numerical age and
 #'   `age_top`. Should be in millions of years before present. `age_top` must
-#'   also be specified, and this must be older than `age_top`.
+#'   also be specified, and this must be younger than `age_bottom`.
 #' @param lithology \code{character}. Filter PBDB collections to those
 #'   containing a named lithology (e.g., "shale", "sandstone").
 #' @param lithology_id \code{integer}. Filter PBDB collections to those
@@ -82,47 +84,51 @@
 #'   stratigraphic name concept containing the specified PBDB collection.
 #'   }
 #'   If \code{sf = TRUE}, an `sf` object is returned instead.
-#' @author Christopher D. Dean
-#' @section Reviewer: N/A
+#' @section Developer(s):
+#'  Christopher D. Dean
+#' @section Reviewer(s):
+#'  Lewis A. Jones
+#'
 #' @details More information can be found for the inputs for this function
 #' using the definition functions (beginning with \code{def_}). Terminology
-#' related to the PBDB can be found at \url{http://paleobiodb.org} or in the
-#' suggested references below.
+#' related to the PBDB can be found at \url{https://paleobiodb.org/#/} or in
+#' the suggested references below.
+#'
 #' @section References:
-#' Peters, S.E. and McClennen, M. (2016). The Paleobiology Database application
-#' programming interface. Paleobiology, 42(1), pp.1-7.
+#' Peters, S.E. and McClennen, M. (2016). The Paleobiology Database
+#' application programming interface. *Paleobiology*, 42(1), pp. 1--7.
+#' \doi{10.1017/pab.2015.39}.
 #'
 #' Uhen, M.D., Allen, B., Behboudi, N., Clapham, M.E., Dunne, E., Hendy, A.,
 #' Holroyd, P.A., Hopkins, M., Mannion, P., Novack-Gottshall, P. and Pimiento,
-#' C. (2023). Paleobiology Database User Guide Version 1.0. PaleoBios, 40(11).
+#' C. (2023). Paleobiology Database User Guide Version 1.0. *PaleoBios*,
+#' 40(11), pp. 1--56. \doi{10.5070/P9401160531}.
+#'
 #' @examples
 #' \dontrun{
-#'   # get_fossils(column_id = 10)
-#'   # head(get_fossils(unit_id = 6279))
-#'   # head(get_fossils(lithology = "sandstone", age_top = 66, age_bottom = 73))
-#'   # head(get_fossils(environ_type = "fluvial", age =  253))
+#'  # Get fossils by Macrostrat column ID
+#'  ex1 <- get_fossils(column_id = 10)
+#'  # Get fossils by Macrostrat unit ID
+#'  ex2 <- get_fossils(unit_id = 6279)
+#'  # Get fossils by lithology and age
+#'  ex3 <- get_fossils(lithology = "sandstone", age_top = 66, age_bottom = 73)
+#'  # Get fossils by environment type and age
+#'  ex4 <- get_fossils(environ_type = "fluvial", age =  253)
 #' }
 #' @export
 #' @family external
-get_fossils <- function(
-    unit_id = NULL,column_id = NULL,
-    interval_name = NULL,age = NULL,age_top = NULL,age_bottom = NULL,
-    lithology = NULL,lithology_id = NULL,lithology_type = NULL,
-    lithology_class = NULL,
-    environ = NULL,environ_id = NULL,environ_type = NULL,environ_class = NULL,
-    econ = NULL,econ_id = NULL,econ_type = NULL,econ_class = NULL,
-    project_id = NULL,strat_name_id = NULL,sf = FALSE) {
+get_fossils <- function(unit_id = NULL, column_id = NULL,
+                        interval_name = NULL, age = NULL,
+                        age_top = NULL, age_bottom = NULL,
+                        lithology = NULL, lithology_id = NULL,
+                        lithology_type = NULL, lithology_class = NULL,
+                        environ = NULL, environ_id = NULL,
+                        environ_type = NULL, environ_class = NULL,
+                        econ = NULL, econ_id = NULL,
+                        econ_type = NULL, econ_class = NULL,
+                        project_id = NULL, strat_name_id = NULL,
+                        sf = FALSE) {
   # Error handling
-  # Check specific argument constraints
-  if (!is.null(age_top) || !is.null(age_bottom)) {
-    if (is.null(age_top) || is.null(age_bottom)) {
-      stop("`age_top` and `age_bottom` must both be specified to filter by an
-           age range.")
-    }
-    if (age_top > age_bottom) {
-      stop("`age_top` must be younger/less than `age_bottom`.")
-    }
-  }
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
@@ -150,7 +156,16 @@ get_fossils <- function(
     sf = "logical"
   )
   check_arguments(x = args, ref = ref)
-
+  # Check specific argument constraints
+  if (!is.null(age_top) || !is.null(age_bottom)) {
+    if (is.null(age_top) || is.null(age_bottom)) {
+      stop("`age_top` and `age_bottom` must both be specified to filter by an
+           age range.")
+    }
+    if (age_top > age_bottom) {
+      stop("`age_top` must be younger/less than `age_bottom`.")
+    }
+  }
   # Recode names
   api_names <- list(column_id = "col_id",
                     lithology = "lith",
@@ -161,17 +176,13 @@ get_fossils <- function(
   rpl <- match(x = names(api_names), table = names(args))
   # Replace names
   names(args)[rpl] <- as.vector(unlist(api_names))
-
   # Set default for format
   if (sf == TRUE) { format <- "geojson"} else { format <- "json" }
-
   # Get request
   dat <- GET_macrostrat(endpoint = "fossils", query = args, format = format)
-
   # Change column names to match PBDB
   names(dat)[names(dat) == "cltn_id"] <- "collection_no"
   names(dat)[names(dat) == "cltn_name"] <- "collection_name"
-
   # Return data
   return(dat)
 }
