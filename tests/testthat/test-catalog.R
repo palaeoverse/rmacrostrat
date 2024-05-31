28230ad5d51b1055bc3f5c4b392879e1b96f04be
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-05-31 11:56:54 +0200

    suggested changes

--- tests/testthat/test-catalog.R
+++ tests/testthat/test-catalog.R
@@ -1,7 +1,9 @@
 test_that("catalog works", {
   rmacrostrat_test_checks(fn = catalog, col_no = 8,
                           check_args = list(type = "grain_sizes"),
-                          fail_args = list(type = "test", list(type = NULL)))
+                          fail_args = list(type = 1,
+                                           type = "test",
+                                           list(type = NULL)))
   rmacrostrat_test_checks(fn = catalog, col_no = 11,
                           check_args = list(type = "minerals"))
 })
