ee92a822471292ed9339f4fe35de07eef55dd34e
Christopher Dean <christopherdaviddean@gmail.com>
2024-06-04 15:40:59 +0200

    REVIEW: def_lithology_att() (#6)
    * Review of def_lithology_att
    
    * Add indent
    
    * Update docs
    
    ---------
    
    Co-authored-by: Bethany Allen <bethany.allen@bsse.ethz.ch>

--- R/def_lithology_att.R
+++ R/def_lithology_att.R
@@ -1,4 +1,5 @@
-#' @title Retrieve lithology attribute definitions
+#' @title Define lithology attributes
+#'
 #' @description A function to retrieve the definitions for one or more
 #'   lithology attributes in the Macrostrat database. By default, all lithology
 #'   attribute definitions are returned.
@@ -9,43 +10,38 @@
 #'   structure") to return a definition for.
 #' @param lithology_att_id \code{integer}. The unique identification number(s)
 #'   of lithology attribute(s) to return a definition for.
-#'
-#' @return A \code{dataframe} containing, for each retrieved core:
-#' \describe{
-#'  \item{lith_att_id}{The unique identifier of the lithology attribute}
-#'  \item{name}{The name of the lithology attribute}
-#'  \item{type}{The type of lithology attribute}
-#'  \item{t_units}{The total number of Macrostrat units to which the lithology
-#'  attribute is attached}
+#' @return A \code{data.frame} containing the following columns:
+#' \itemize{
+#'   \item \code{lith_att_id}: The unique identifier of the lithology attribute.
+#'   \item \code{name}: The name of the lithology attribute.
+#'   \item \code{type}: The lithology attribute type.
+#'   \item \code{t_units}: The total number of Macrostrat units to which the
+#'    lithology attribute is attached.
 #'  }
-#'
-#' @author Bethany Allen
-#'
+#' @section Developer(s):
+#'  Bethany Allen
+#' @section Reviewer(s):
+#'  Christopher D. Dean
 #' @examples
 #' \dontrun{
-#' if (interactive()) {
-#'   # lithology_attributes <- def_lithology_att()
-#'   # lithology_attributes <- def_lithology_att(att_type = "sed structure")
-#' }
+#' # Return all lithology attribute definitions
+#' ex1 <- def_lithology_att()
+#' # Return subsets of lithology attribute definitions
+#' ex2 <- def_lithology_att(att_type = "sed structure")
 #' }
 #' @export
 #' @family defs_feat
-def_lithology_att <- function(
-    lithology_att = NULL,
-    att_type = NULL,
-    lithology_att_id = NULL) {
-
+def_lithology_att <- function(lithology_att = NULL,
+                              att_type = NULL,
+                              lithology_att_id = NULL) {
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
-  ref <- list(
-    lithology_att = "character",
-    att_type = "character",
-    lithology_att_id = "integer"
+  ref <- list(lithology_att = "character", att_type = "character",
+              lithology_att_id = "integer"
   )
   check_arguments(x = args, ref = ref)
-
   # Recode names
   api_names <- list(lithology_att = "lith_att",
                     lithology_att_id = "lith_att_id")
@@ -53,14 +49,9 @@ def_lithology_att <- function(
   rpl <- match(x = names(api_names), table = names(args))
   # Replace names
   names(args)[rpl] <- as.vector(unlist(api_names))
-
-  # Set default for format
-  format <- "json"
-
   # Get request
   dat <- GET_macrostrat(endpoint = "defs/lithology_attributes", query = args,
-                        format = format)
-
+                        format = "json")
   # Return data
   return(dat)
 }
