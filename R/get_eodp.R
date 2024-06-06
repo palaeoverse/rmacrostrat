e97f40aa6ca3c9b751b3feee5cebfd3933e4c91b
William Gearty <willgearty@gmail.com>
2024-06-06 12:34:49 -0400

    REVIEW: get_eodp() (#23)
    * Review get_eodp
    
    * Stop restricting program names
    
    * Update tests
    
    ---------
    
    Co-authored-by: Bethany Allen <bethany.allen@bsse.ethz.ch>

--- R/get_eodp.R
+++ R/get_eodp.R
@@ -1,10 +1,11 @@
 #' @title Retrieve core data from ocean drilling programs
-#' @description Call data from the
+#'
+#' @description A function to retrieve data from the
 #' [Extending Ocean Drilling Pursuits (eODP)](https://eodp.github.io)
 #' project, a collation of sedimentary description data
-#' from ocean drilling cores. This includes cores from the Deep Sea Drilling
-#' Project (DSDP), Integrated Ocean Drilling Program (IODP), and Ocean Drilling
-#' Program (ODP).
+#' from ocean drilling cores. This currently includes cores from the Deep Sea
+#' Drilling Project (DSDP), Integrated Ocean Drilling Program (IODP), and Ocean
+#' Drilling Program (ODP).
 #'
 #' @param column_id \code{integer}. Filter cores by their unique identification
 #'   number(s).
@@ -17,27 +18,33 @@
 #' @param sf \code{logical}. Should the results be returned as an `sf` object?
 #'   Defaults to `FALSE`.
 #'
-#' @return A \code{dataframe} containing, for each retrieved core:
-#' \describe{
-#'  \item{col_group}{The name of the drilling program and leg/expedition}
-#'  \item{site_hole}{The name of the drilling site and hole}
-#'  \item{date_started}{The date on which drilling commenced}
-#'  \item{ref_id}{The unique identifier of the reference}
-#'  \item{col_id}{The unique identifier of the Macrostrat column}
-#'  \item{lat}{Decimal degree latitude of the core}
-#'  \item{lng}{Decimal degree longitude of the core}
-#'  \item{top_depth}{A vector describing the height of the top of each unit}
-#'  \item{bottom_depth}{A vector describing the height of the bottom of each
-#'    unit}
-#'  \item{primary_lith}{A vector giving the name of the primary lithology of
-#'    each unit}
-#'  \item{lith_id}{A vector giving the unique identifier of the primary
-#'    lithology of each unit}
-#'  \item{minor_lith}{A vector giving the name of the primary lithology of
-#'    each unit}
-#'  }
-#'  If sf = TRUE, an `sf` object is outputted instead.
-#' @author Bethany Allen
+#' @return A \code{data.frame} containing, for each retrieved core:
+#' \itemize{
+#'   \item \code{col_group}: The name of the drilling program and
+#'     leg/expedition.
+#'   \item \code{site_hole}: The name of the drilling site and hole.
+#'   \item \code{date_started}: The date on which drilling commenced.
+#'   \item \code{ref_id}: The unique identifier of the reference.
+#'   \item \code{col_id}: The unique identifier of the Macrostrat column.
+#'   \item \code{lat}: Decimal degree latitude of the core.
+#'   \item \code{lng}: Decimal degree longitude of the core.
+#'   \item \code{top_depth}: A vector describing the height of the top of each
+#'     unit.
+#'   \item \code{bottom_depth}: A vector describing the height of the bottom of
+#'     each unit.
+#'   \item \code{primary_lith}: A vector giving the name of the primary
+#'     lithology of each unit.
+#'   \item \code{lith_id}: A vector giving the unique identifier of the primary
+#'     lithology of each unit.
+#'   \item \code{minor_lith}: A vector giving the name of the primary lithology
+#'     of each unit.
+#' }
+#'   If `sf` is `TRUE`, an `sf` object is returned instead, with a "geometry"
+#'   column that contains the spatial data instead of the `lat`/`lng` columns.
+#' @section Developer(s):
+#'   Bethany Allen
+#' @section Reviewer(s):
+#'   William Gearty
 #' @details More information can be found about the inputs for this function by
 #'  using [def_drilling_sites()].
 #' @section References:
@@ -48,53 +55,37 @@
 #'
 #' @examples
 #' \dontrun{
-#' if (interactive()) {
-#'   # cores <- get_eodp(column_id = c(5081, 5082))
-#'   # cores <- get_eodp(site = "U1351")
-#'   # cores <- get_eodp(leg = "317")
-#'   # cores <- get_eodp(program = "IODP")
-#' }
+#' # Get data for specific cores
+#' cores <- get_eodp(column_id = c(5081, 5082))
+#' # Get data for all cores at a specific site
+#' cores <- get_eodp(site = "U1351")
+#' # Get data for all cores for a specific leg
+#' cores <- get_eodp(leg = "317")
+#' # Get data for all cores for a specific program
+#' cores <- get_eodp(program = "IODP")
 #' }
 #' @export
 #' @family external
-get_eodp <- function(
-    column_id = NULL,
-    site = NULL,
-    leg = NULL,
-    program = NULL,
-    sf = FALSE) {
+get_eodp <- function(column_id = NULL, site = NULL, leg = NULL, program = NULL,
+                     sf = FALSE) {
 
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
-  ref <- list(
-    column_id = "integer",
-    site = "character",
-    leg = "character",
-    program = "character",
-    sf = "logical"
-  )
+  ref <- list(column_id = "integer", site = "character", leg = "character",
+              program = "character", sf = "logical")
   check_arguments(x = args, ref = ref)
-  # Check whether provided program fits categories
-  if (!is.null(program) && (program != "DSDP" && program != "IODP" &&
-                            program != "ODP")) {
-    stop("program must either be 'DSDP', 'IODP', or 'ODP'")
-  }
-
   # Recode names
   api_names <- list(column_id = "col_id", program = "epoch")
   # Match names
   rpl <- match(x = names(api_names), table = names(args))
   # Replace names
   names(args)[rpl] <- as.vector(unlist(api_names))
-
   # Set default for format
   if (sf) format <- "geojson" else format <- "json"
-
   # Get request
   dat <- GET_macrostrat(endpoint = "eodp", query = args, format = format)
-
   # Return data
   return(dat)
 }
