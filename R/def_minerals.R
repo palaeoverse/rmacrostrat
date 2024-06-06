0a47eca753e174c0426b4890b4a90a9c0eb9498b
Bethany Allen <bethany.allen@bsse.ethz.ch>
2024-06-06 18:32:37 +0200

    Typos, tabs, remove breaks

--- R/def_minerals.R
+++ R/def_minerals.R
@@ -14,19 +14,18 @@
 #'
 #' @return A \code{data.frame} containing the following columns:
 #' \itemize{
-#'   \item \code{mineral_id}: The unique identification number(s) of the
-#'    mineral(s).
+#'   \item \code{mineral_id}: The unique identification number of the mineral.
 #'   \item \code{mineral}: The name of the mineral.
 #'   \item \code{mineral_type}: The name of the type of mineral.
 #'   \item \code{formula}: The chemical formula of the mineral.
 #'   \item \code{formula_tags}: The chemical formula of the mineral with
-#'   appropriate sub/superscript html tags.
+#'    appropriate sub/superscript html tags.
 #'   \item \code{url}: A URL for the mineral's entry in
-#'   [Mindat](https://www.mindat.org).
+#'    [Mindat](https://www.mindat.org).
 #'   \item \code{hardness_min}: The minimum hardness value (Mohs Hardness
-#'   Scale) of the mineral.
+#'    Scale) of the mineral.
 #'   \item \code{hardness_max}: The maximum hardness value (Mohs Hardness
-#'   Scale) of the mineral.
+#'    Scale) of the mineral.
 #'   \item \code{crystal_form}: The crystal form of the mineral.
 #'   \item \code{mineral_color}: A description of the color of the mineral.
 #'   \item \code{lustre}: A description of the lustre of the mineral.
@@ -46,22 +45,18 @@
 #' }
 #' @export
 #' @family defs_feat
-def_minerals <- function(mineral = NULL,
-                         mineral_type = NULL,
-                         element = NULL) {
+def_minerals <- function(mineral = NULL, mineral_type = NULL, element = NULL) {
   # Error handling
   # Collect input arguments as a list
   args <- as.list(environment())
   # Check whether class of arguments is valid
-  ref <- list(
-    mineral = "character",
-    mineral_type = "character",
+  ref <- list(mineral = "character", mineral_type = "character",
     element = "character"
   )
   check_arguments(x = args, ref = ref)
   # Get request
-  dat <- GET_macrostrat(endpoint = "defs/minerals",
-                        query = args, format = "json")
+  dat <- GET_macrostrat(endpoint = "defs/minerals", query = args,
+                        format = "json")
   # Return data
   return(dat)
 }
