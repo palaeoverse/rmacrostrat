4dcd59438f61ef2946d6de0df953d06d15c86bea
Bethany Allen <bethany.allen@bsse.ethz.ch>
2024-06-11 12:03:04 +0200

    Review def_lithologies

--- R/def_lithologies.R
+++ R/def_lithologies.R
@@ -1,25 +1,25 @@
-#' @title Retrieve lithology definitions
-#' @description A function to retrieve the definitions for one or more
-#'   lithologies in the Macrostrat database. By default, all lithology
-#'   definitions are returned.
-#' @param lithology \code{character}. The name of a lithology (e.g.,
+#' @title Define lithologies
+#' @description A function to retrieve the definitions of one or more
+#'   lithologies in the Macrostrat database. If no arguments are specified
+#'   (the default), all lithology definitions are returned.
+#' @param lithology \code{character}. The name of the desired lithology (e.g.,
 #'   "limestone") to return a definition for.
-#' @param lithology_group \code{character}. The name of a lithological group
-#'   (e.g., "unconsolidated") to return a definition for.
-#' @param lithology_class \code{character}. The name of a lithological class
-#'   (e.g., "sedimentary") to return a definition for.
-#' @param lithology_type \code{character}. The name of a lithological type
-#'   (e.g., "siliciclastic") to return a definition for.
+#' @param lithology_group \code{character}. The name of the desired lithological
+#'   group (e.g., "unconsolidated") to return a definition for.
+#' @param lithology_class \code{character}. The name of the desired lithological
+#'   class (e.g., "sedimentary") to return a definition for.
+#' @param lithology_type \code{character}. The name of the desired lithological
+#'   type (e.g., "siliciclastic") to return a definition for.
 #' @param lithology_id \code{integer}. The unique identification number(s) of
 #'   one or more lithologies to return a definition for.
 #' @return A `data.frame` containing the following columns:
 #' \itemize{
-#'   \item \code{lith_id}: Unique ID of the lithology.
+#'   \item \code{lith_id}: The unique identification number of the lithology.
 #'   \item \code{name}: The name of the lithology.
-#'   \item \code{type}: Lithology type, less inclusive than class.
-#'   \item \code{group}: Lithology group, less inclusive than type.
-#'   \item \code{class}: Lithology class, more inclusive than type.
-#'   \item \code{color}: Recommended coloring for lithology.
+#'   \item \code{type}: The lithology type, less inclusive than class.
+#'   \item \code{group}: The lithology group, less inclusive than type.
+#'   \item \code{class}: The lithology class, more inclusive than type.
+#'   \item \code{color}: The recommended coloring for the lithology.
 #'   \item \code{fill}: The code for the fill pattern as established in the
 #'     [Federal Geographic Data Committee](https://ngmdb.usgs.gov/fgdc_gds/)'s
 #'     [Digital Cartographic Standard for Geologic Map
@@ -27,14 +27,19 @@
 #'   \item \code{t_units}: The total number of Macrostrat units that are
 #'     partially or entirely composed of the lithology.
 #' }
-#' @author William Gearty
+#' @section Developer(s):
+#'   William Gearty
+#' @section Reviewer(s):
+#'   Bethany Allen
 #' @examples
 #' \dontrun{
 #' # return all lithology definitions
 #' ex1 <- def_lithologies()
-#' # return subsets of lithology definitions
+#' # return a definition for sandstone
 #' ex2 <- def_lithologies(lithology = "sandstone")
+#' # return definitions for lithologies of a specific type
 #' ex3 <- def_lithologies(lithology_type = "sedimentary")
+#' # return definitions for lithologies using their unique ID numbers
 #' ex4 <- def_lithologies(lithology_id = c(1,5))
 #' }
 #' @export
