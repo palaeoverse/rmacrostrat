451e6e72bbb78e34e65264856ae2e2f3ea24c511
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-06-11 17:43:21 +0200

    update logo and strat_column

--- setup/strat_column.R
+++ setup/strat_column.R
@@ -6,26 +6,25 @@ library(deeptime)
 # Get the column definition of the San Juan Basin
 column_def <- def_columns(column_name = "San Juan Basin")
 # Using the column ID, retrieve all units of Cretaceous age
-units <- get_units(column_id = column_def$col_id, interval_name = "Cretaceous")
-# Plot stratigraphic log
-ggplot(units, aes(ymin = b_age, ymax = t_age,
-                  xmin = 0, xmax = 1, fill = unit_name, color = unit_name)) +
+units <- get_units(column_id = column_def$col_id,
+                   interval_name = "Cretaceous")
+# Add midpoint age for plotting
+units$m_age <- (units$b_age + units$t_age) / 2
+# Plot stratigraphic column
+ggplot(units, aes(ymin = b_age, ymax = t_age, xmin = 0, xmax = 1)) +
   # Plot units
-  geom_rect(alpha = 0.5, colour = "grey30", linetype = 2) +
+  geom_rect(alpha = 0.5, fill = units$color, color = "black",
+            linetype = 2) +
   # Add text labels
-  geom_text_repel(aes(x = 1, y = (b_age + t_age) / 2, label = unit_name),
-                  color = "black",
-                  min.segment.length = 0, size = 3,
-                  nudge_x = rep_len(x = c(0.5, 1.5), length.out = 17),
-                  direction = "x", hjust = 0, force = TRUE) +
-  #geom_linerange(aes(ymin = b_age, ymax = t_age, x = rep_len(x = c(0.05, 0.95), length.out = 17)), linetype = 1, linewidth = 1.5) +
-  geom_rect(aes(ymin = b_age, ymax = t_age,
-                xmin = rep_len(x = c(0.9), length.out = 17),
-                xmax = rep_len(x = c(1), length.out = 17)), color = "black") +
+  geom_text_repel(aes(x = 1, y = m_age, label = unit_name),
+                  size = 4, hjust = 0,
+                  min.segment.length = 0, direction = "x",
+                  nudge_x = rep_len(x = c(2, 3), length.out = 17)) +
+  # Add additional unit rectangles
+  geom_rect(aes(ymin = b_age, ymax = t_age, xmin = 0.9, xmax = 1),
+            fill = units$color, color = "black") +
+  # Reverse direction of y-axis
   scale_y_reverse(n.breaks = 10, name = "Time (Ma)") +
-  # Fill color
-  scale_fill_manual(values = units$color) +
-  scale_color_manual(values = units$color) +
   # Theming
   theme_classic() +
   theme(legend.position = "none",
@@ -34,7 +33,8 @@ ggplot(units, aes(ymin = b_age, ymax = t_age,
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank()) +
   # Add geological time scale
-  coord_geo(pos = "left", dat = list("stages"),
-            rot = 90, abbrv = FALSE, size = 2, expand = FALSE)
+  coord_geo(pos = "left", dat = list("stages"), rot = 90)
 
-ggsave("./setup/strat_column.png", width = 150, height = 300, dpi = 300, units = "mm")
+# Save
+ggsave("./setup/strat_column.png", width = 213, height = 400, dpi = 300, units = "mm",
+       scale = 0.9)
