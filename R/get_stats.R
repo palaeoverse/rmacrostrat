fdc9e14cba788be4cff9f72426f3a83aaf683b61
Bethany Allen <39991015+bethany-j-allen@users.noreply.github.com>
2024-06-11 15:26:38 +0100

    REVIEW: get_stats() (#32)
    * Review get_stats
    
    * Clarify that there are no arguments
    
    ---------
    
    Co-authored-by: William Gearty <willgearty@gmail.com>

--- R/get_stats.R
+++ R/get_stats.R
@@ -1,9 +1,11 @@
 #' @title Retrieve Macrostrat database statistics
 #' @description A function to retrieve various statistics about each Macrostrat
-#'   project in the Macrostrat database.
+#'   project in the Macrostrat database. Function is called without
+#'   user-specified arguments.
 #' @return A `data.frame` containing the following columns:
 #' \itemize{
-#'   \item \code{project_id}: Unique ID of the Macrostrat project.
+#'   \item \code{project_id}: The unique identification number of the Macrostrat
+#'     project.
 #'   \item \code{project}: The name of the Macrostrat project.
 #'   \item \code{columns}: The number of Macrostrat columns associated with the
 #'     project.
@@ -18,10 +20,14 @@
 #'   \item \code{t_polys}: The number of spatial polygons associated with the
 #'     project.
 #' }
-#' @author William Gearty
+#' @section Developer(s):
+#' William Gearty
+#' @section Reviewer(s):
+#' Bethany Allen
 #' @examples
 #' \dontrun{
-#' get_stats()
+#' # Return all project statistics
+#' ex1 <- get_stats()
 #' }
 #' @export
 #' @family meta
@@ -29,7 +35,6 @@ get_stats <- function() {
   # Get request
   dat <- GET_macrostrat(endpoint = "stats", query = list(all = TRUE),
                         format = "json")
-
   # Return data
   return(dat)
 }
