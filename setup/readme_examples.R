f152eb60a47237d9081a31702a71fc956d8b1078
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-07-22 12:44:20 +0200

    Adjust column

--- setup/readme_examples.R
+++ setup/readme_examples.R
@@ -27,9 +27,7 @@ ggplot(san_juan_units, aes(ymin = b_age, ymax = t_age,
   geom_rect(fill = san_juan_units$color, color = "black") +
   # Add text labels
   geom_text_repel(aes(x = x_max, y = m_age, label = unit_name),
-                  size = 3.5, hjust = 0, force = 2,
-                  min.segment.length = 0, direction = "y",
-                  nudge_x = rep_len(x = c(2, 3), length.out = 17)) +
+                  size = 3.5, nudge_x = 1.5) +
   # Reverse direction of y-axis
   scale_y_reverse(limits = c(145, 66), n.breaks = 10, name = "Time (Ma)") +
   # Theming
