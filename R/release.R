7ad9c4d9a8b63166ed94d489e1d547a85507d915
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2024-09-25 14:59:54 +0100

    Update release.R

--- R/release.R
+++ R/release.R
@@ -1,5 +1,5 @@
 release_questions <- function() {
   c(
-    "Did you re-build the vignettes using `vignettes/source/precompile.R`?"
+    "Did you re-build the vignettes using `vignettes/precompile.R`?"
   )
 }
